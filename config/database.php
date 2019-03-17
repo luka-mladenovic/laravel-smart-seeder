c89a90ad6962ef7a407766948cdf8cdcac94c86a
luka-mladenovic <luka@kreativika.si>
2019-03-17 17:39:42 +0100

    Add service provider

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Seeder Repository Table
+    |--------------------------------------------------------------------------
+    |
+    | This table keeps track of all the seeds that have already run for
+    | your application. Using this information, we can determine which of
+    | the seeds on disk haven't actually been run in the database.
+    |
+    */
+
+    'seeds' => 'seeds',
+];
---
+++ src/SmartSeederServiceProvider.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace Lukam\SmartSeeder;
+
+use Lukam\SmartSeeder\Console;
+use Lukam\SmartSeeder\Seeds\Seeder;
+use Illuminate\Support\ServiceProvider;
+use Lukam\SmartSeeder\Seeds\SeedCreator;
+use Lukam\SmartSeeder\Seeds\DatabaseSeedRepository;
+
+class SmartSeederServiceProvider extends ServiceProvider
+{
+    protected $commands = [
+        'SeedRun'       => 'command.seed.run',
+        'SeedReset'     => 'command.seed.reset',
+        'SeedStatus'    => 'command.seed.status',
+        'SeedInstall'   => 'command.seed.install',
+        'SeedRefresh'   => 'command.seed.refresh',
+        'SeedRollback'  => 'command.seed.rollback',
+    ];
+
+    protected $devCommands = [
+        'SeedMake'      => 'command.seed.make',
+    ];
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/database.php', 'database'
+        );
+
+        $this->registerRepository();
+
+        $this->registerSeeder();
+
+        $this->registerCreator();
+
+        $this->registerCommands(array_merge(
+            $this->commands, $this->devCommands
+        ));
+    }
+
+    /**
+     * Register the seeder repository service.
+     *
+     * @return void
+     */
+    protected function registerRepository()
+    {
+        $this->app->singleton('seed.repository', function ($app) {
+            $table = $app['config']['database.seeds'];
+
+            return new DatabaseSeedRepository($app['db'], $table);
+        });
+    }
+
+    /**
+     * Register the seeder service.
+     *
+     * @return void
+     */
+    protected function registerSeeder()
+    {
+        // The seeder is responsible for actually running and rollback the seeder
+        // files in the application. We'll pass in our database connection resolver
+        // so the seeder can resolve any of these connections when it needs to.
+        $this->app->singleton('seeder', function ($app) {
+            $repository = $app['seed.repository'];
+
+            return new Seeder($repository, $app['db'], $app['files']);
+        });
+    }
+
+    /**
+     * Register the seed creator.
+     *
+     * @return void
+     */
+    protected function registerCreator()
+    {
+        $this->app->singleton('seed.creator', function ($app) {
+            return new SeedCreator($app['files']);
+        });
+    }
+
+    /**
+     * Register the given commands.
+     *
+     * @param  array  $commands
+     * @return void
+     */
+    protected function registerCommands($commands)
+    {
+        foreach (array_keys($commands) as $command) {
+            call_user_func_array([$this, "register{$command}Command"], []);
+        }
+        $this->commands(array_values($commands));
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerSeedInstallCommand()
+    {
+        $this->app->singleton('command.seed.install', function ($app) {
+            return new Console\InstallCommand($app['seed.repository']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerSeedMakeCommand()
+    {
+        $this->app->singleton('command.seed.make', function ($app) {
+            return new Console\SeedMakeCommand($app['seed.creator'], $app['composer']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerSeedStatusCommand()
+    {
+        $this->app->singleton('command.seed.status', function ($app) {
+            return new Console\StatusCommand($app['seeder']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerSeedRunCommand()
+    {
+        $this->app->singleton('command.seed.run', function ($app) {
+            return new Console\SeedCommand($app['seeder']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerSeedRollbackCommand()
+    {
+        $this->app->singleton('command.seed.rollback', function ($app) {
+            return new Console\RollbackCommand($app['seeder']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerSeedResetCommand()
+    {
+        $this->app->singleton('command.seed.reset', function ($app) {
+            return new Console\ResetCommand($app['seeder']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerSeedRefreshCommand()
+    {
+        $this->app->singleton('command.seed.refresh', function ($app) {
+            return new Console\RefreshCommand($app['seeder']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array_merge([
+            'seeder', 'seed.repository', 'seed.creator',
+        ],array_values($this->commands), array_values($this->devCommands));
+    }
+}
